c3c444c269e97e3ff9d269a30d3c147a83711803
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-06-16 12:33:44 +0200

    Sleep to prevent 'server has gone away'

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git clone https://github.com/AnonymusBadger/mediaflex_recruitment_task.git &&
     cd mediaflex_recruitment_task/ &&
     docker-compose up -d &&
     symfony composer install &&
-    symfony console doct:mig:mig --no-interaction &&
     php bin/console lexik:jwt:generate-keypair &&
+    sleep 10 &&
+    symfony console doct:mig:mig --no-interaction &&
     symfony console doct:fix:load -n
